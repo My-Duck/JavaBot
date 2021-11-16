14a7502afa4525e8ea0747fbe8df9fb7dab8f473
Dima <dimazavr007@gmail.com>
2021-11-16 23:57:16 +0300

    Попробовал брать анекдоты с башорга. Изменил логику.

--- actualbot-main/bot/src/com/company/TelegramBot.java
+++ actualbot-main/bot/src/com/company/TelegramBot.java
@@ -1,4 +1,56 @@
 package com.company;
 
-public class TelegramBot {
-}
+import com.company.api.State;
+import lombok.SneakyThrows;
+import org.telegram.telegrambots.bots.DefaultAbsSender;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Hashtable;
+import java.util.Optional;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+    public NewBotLogic logic;
+    public TelegramBot (NewBotLogic logic){
+        this.logic = logic;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "@RCSJBot";
+    }
+
+    @Override
+    public String getBotToken(){
+        return "2002771093:AAH8V5FggnFC6otGQRE4vM8J69Nqq2s7DB8";
+    }
+
+
+    @SneakyThrows
+    public void messageHandler(Message message){
+        if (message.hasText()) {
+                String id = message.getChatId().toString();
+                execute(
+                        SendMessage.builder()
+                                .text(logic.handleMessage(message.getText(),id))
+                                .chatId(id)
+                                .build());
+        }
+    }
+
+    @SneakyThrows
+    @Override
+    public void onUpdateReceived(Update update) {
+       if (update.hasMessage()){
+           messageHandler(update.getMessage());
+           }
+       }
+    }
--- actualbot-main/bot/src/com/company/TelegramMain.java
+++ actualbot-main/bot/src/com/company/TelegramMain.java
@@ -1,58 +1,13 @@
 package com.company;
 
-import com.company.api.State;
 import lombok.SneakyThrows;
-import org.telegram.telegrambots.bots.DefaultAbsSender;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.MessageEntity;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.util.Hashtable;
-import java.util.Optional;
-
-public class TelegramMain extends TelegramLongPollingBot {
-
-    public BotLogic logic = new BotLogic();
-    @Override
-    public String getBotUsername() {
-        return "@RCSJBot";
-    }
-
-    @Override
-    public String getBotToken(){
-        return "8";
-    }
-
+public class TelegramMain {
     @SneakyThrows
     public static void main(String[] args) {
-        TelegramMain bot = new TelegramMain();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        telegramBotsApi.registerBot(bot);
+        telegramBotsApi.registerBot(new TelegramBot(new NewBotLogic()));
     }
-
-    @SneakyThrows
-    public void messageHandler(Message message){
-        if (message.hasText()) {
-                String id = message.getChatId().toString();
-                execute(
-                        SendMessage.builder()
-                                .text(logic.handleMessage(message.getText(),id))
-                                .chatId(id)
-                                .build());
-        }
-    }
-
-    @SneakyThrows
-    @Override
-    public void onUpdateReceived(Update update) {
-       if (update.hasMessage()){
-           messageHandler(update.getMessage());
-           }
-       }
-    }
+}
